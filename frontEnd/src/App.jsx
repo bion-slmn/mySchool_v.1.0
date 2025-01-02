147a914103322abd181bdea4c533c72ab217572e
bion-slmn <bionsol25@gmail.com>
2025-01-02 13:04:58 +0300

    added private routes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -18,11 +18,12 @@ import { ViewStudent } from "./components/students/viewStudent";
 import FeesAndPayments from "./pages/fees";
 import HomePage from "./pages/home";
 import Dashboard from "./pages/dashboard";
-import {ViewPaymentonFee} from "./components/payments/viewPayments";
+import { ViewPaymentonFee } from "./components/payments/viewPayments";
 import CreateStudent from "./components/students/createStudent";
 import CreateFee from "./components/fees/createFee";
 import CreatePayments from "./components/payments/createPayments";
 import CreateTerm from "./components/school/createTerm";
+import PrivateRoute from "./components/privateRoute";
 
 function App() {
   return (
@@ -30,28 +31,30 @@ function App() {
       <Router>
         <AuthProvider>
           <Navigationbar />
-          
+
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="login" element={<Login />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="createStudent" element={<CreateStudent />} />
-            <Route path="createFee" element={<CreateFee />} />
-            <Route path="createPayments" element={<CreatePayments />} />
-            <Route path="updateSchool" element={<UpdateSchool/>} />
-            <Route path="createTerm" element={<CreateTerm />} />
-            <Route path="school" element={<School />} />
-            <Route path="updateTerm" element={<UpdateTerm />} />
-            <Route path="grades" element={<Grades />} />
-            <Route path="viewStudents" element={<ViewStudentsinGrade />} />
-            <Route path="viewPaymentsonFee" element={<ViewPaymentonFee />} />
-            <Route path="updateGrade" element={<UpdateGrades />} />
-            <Route path="updateStudent" element={<UpdateStudent />} />
-            <Route path="viewPayments" element={<ViewPaymentonStudent />} />
-            <Route path="/viewPayments/PaymentDetails" element={<ViewPaymentDetails />} />
-            <Route path="viewPayments/PaymentDetails/updatePayment" element={<UpdatePayment />} />
-            <Route path="viewStudent" element={<ViewStudent />} />
-            <Route path="viewFee" element={<FeesAndPayments />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="createStudent" element={<CreateStudent />} />
+              <Route path="createFee" element={<CreateFee />} />
+              <Route path="createPayments" element={<CreatePayments />} />
+              <Route path="updateSchool" element={<UpdateSchool />} />
+              <Route path="createTerm" element={<CreateTerm />} />
+              <Route path="school" element={<School />} />
+              <Route path="updateTerm" element={<UpdateTerm />} />
+              <Route path="grades" element={<Grades />} />
+              <Route path="viewStudents" element={<ViewStudentsinGrade />} />
+              <Route path="viewPaymentsonFee" element={<ViewPaymentonFee />} />
+              <Route path="updateGrade" element={<UpdateGrades />} />
+              <Route path="updateStudent" element={<UpdateStudent />} />
+              <Route path="viewPayments" element={<ViewPaymentonStudent />} />
+              <Route path="/viewPayments/PaymentDetails" element={<ViewPaymentDetails />} />
+              <Route path="viewPayments/PaymentDetails/updatePayment" element={<UpdatePayment />} />
+              <Route path="viewStudent" element={<ViewStudent />} />
+              <Route path="viewFee" element={<FeesAndPayments />} />
+            </Route>
           </Routes>
         </AuthProvider>
       </Router>
