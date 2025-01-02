ae85c0021ba88b284492dabcf116df4a9bbbd585
bion-slmn <bionsol25@gmail.com>
2025-01-02 10:59:02 +0300

    updated settings

--- backend/config/settings/__init__.py
+++ backend/config/settings/__init__.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+print(os.getenv('DJANGO_ENV'), 111111111111111111)
 if os.getenv('DJANGO_ENV') == 'production':
     from .production import *
 else:
