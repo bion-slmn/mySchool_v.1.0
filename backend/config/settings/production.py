71d3b294510b63025cb92da9e680288aadf96209
bion-slmn <bionsol25@gmail.com>
2025-01-02 11:13:01 +0300

    updated front end

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -7,8 +7,9 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+url = "postgresql://schoool_test_user:ulWj70BIZM0Y68GmrPUpNfrDbSWGzWby@dpg-ctqp845ds78s73dgtkt0-a.oregon-postgres.render.com/schoool_test"
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(url)
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
