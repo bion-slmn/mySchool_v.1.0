cead1795fc6a7cfc8dfbec3566c28addfa802811
bion-slmn <bionsol25@gmail.com>
2025-01-07 19:46:44 +0300

    update the backend

--- frontEnd/src/service/apiservice.js
+++ frontEnd/src/service/apiservice.js
@@ -1,6 +1,6 @@
 import errorHandler from "./errorHandler";
 
-export const API_URL = "http://127.0.0.1:8000/api/";
+export const API_URL = "https://myschool-v-1-0.onrender.com/api/";
 
 // function to get data from backend
 export const fetchData = async (endPoint) => {
